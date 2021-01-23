499209e6ccdf36d96fe08e7ebd0cc6932d2551e2
Alvaro Olea Rojas <alvaroolear@gmail.com>
2021-01-23 19:38:30 +0100

    Ordered by points quantity

--- functions/google.js
+++ functions/google.js
@@ -1,5 +1,5 @@
 const { GoogleSpreadsheet } = require('google-spreadsheet');
-const cellsGrid = { startRowIndex: 3, endRowIndex: 50, startColumnIndex:0, endColumnIndex: 18 };
+const cellsGrid = { startRowIndex: 3, endRowIndex: 50, startColumnIndex: 0, endColumnIndex: 18 };
 
 async function connect() {
 	// Responsible for accessing the spreadsheet
@@ -16,7 +16,7 @@ async function connect() {
 
 		return doc;
 	}
-	catch(e) {
+	catch (e) {
 		console.error(e);
 		return false;
 	}
@@ -24,9 +24,9 @@ async function connect() {
 
 async function getAllMembersData() {
 	const doc = await connect();
-	const membersData = {};
+	const membersData = [];
 
-	if(!doc) return false;
+	if (!doc) return false;
 
 	try {
 		// Get first sheet of the document
@@ -38,7 +38,8 @@ async function getAllMembersData() {
 		for (let rowIndex = cellsGrid.startRowIndex; rowIndex < cellsGrid.endRowIndex; rowIndex++) {
 			if (sheet.getCell(rowIndex, 1).value === null) continue;
 
-			membersData[sheet.getCell(rowIndex, 1).value] = {
+			membersData.push({
+				name: sheet.getCell(rowIndex, 1).value,
 				weekly_points: sheet.getCell(rowIndex, 7).value,
 				monthly_points_w1: sheet.getCell(rowIndex, 9).value,
 				monthly_points_w2: sheet.getCell(rowIndex, 10).value,
@@ -46,12 +47,23 @@ async function getAllMembersData() {
 				monthly_points_w4: sheet.getCell(rowIndex, 12).value,
 				total_points: sheet.getCell(rowIndex, 14).value,
 				interview: (sheet.getCell(rowIndex, 16).value !== null) ? true : false,
-			};
+			});
 		}
+
+		membersData.sort(function(a, b) {
+			if (a.total_points > b.total_points) {
+				return -1;
+			}
+			if (a.total_points < b.total_points) {
+				return 1;
+			}
+			// a must be equal to b
+			return 0;
+		});
 
 		return membersData;
 	}
-	catch(e) {
+	catch (e) {
 		console.error(e);
 		return false;
 	}
@@ -59,9 +71,14 @@ async function getAllMembersData() {
 
 async function getMemberData(memberName) {
 	const members = await getAllMembersData();
-	console.log(memberName);
 
-	return await (memberName in members) ? members[memberName] : false;
+	for (const [key, member] of Object.entries(members)) {
+		if (Object.values(member).indexOf(memberName) > -1) {
+			return member;
+		}
+	}
+
+	return false;
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const Discord = require('discord.js');
-const googleSheet = require('./google.js');
+const google = require('./functions/google.js');
 
 const client = new Discord.Client();
-const prefix = '!puntos';
+const prefix = '!';
+const commandList = ['!puntos'];
 
 client.on('ready', () => {
 	console.log('Bot Now connected!');
@@ -16,80 +17,83 @@ client.on('message', message => {
 
 	const args = message.content.trim().split(/ +/);
 	const command = args.shift().toLowerCase();
+	console.log(command);
+
+	if (!commandList.includes(command)) return;
 
 	const nick = message.member.nickname !== null ? message.member.nickname : message.author.username;
 
-	if (!args.length) {
-		// No parameters, returns points of the user
-		googleSheet.getMemberData(nick).then(function(val) {
-			console.log(val);
-			if(!val) {
-				message.channel.send(`${message.author}, no te hemos encontrado en la lista de puntos. :sob:`);
-			}
-			else {
-				message.channel.send(`¡${message.author}, tienes ${val.total_points} puntos acumulados!`);
-			}
-		}, function(e) {
-			console.error(e);
-		});
-	}
-	else if (args[0] === 'detalle') {
-		googleSheet.getMemberData(nick).then(function(val) {
-			console.log(val);
-			if(!val) {
-				message.channel.send(`${message.author}, no te hemos encontrado en la lista de puntos. :sob:`);
-			}
-			else {
-				const fields = [
-					{ name: 'Puntos totales', value: val.total_points, inline: true },
-					{ name: 'Puntos semanales', value: val.weekly_points, inline: true },
-				];
-
-				if(val.interview) {
-					fields.push({ name: 'Entrevista realizada', value: 'Sí' });
+	if(command === '!puntos') {
+		if (!args.length) {
+			// No parameters, returns points of the user
+			google.getMemberData(nick).then(function(val) {
+				if(!val) {
+					message.channel.send(`${message.author}, no te hemos encontrado en la lista de puntos. :sob:`);
 				}
-
-				const embed = new Discord.MessageEmbed()
-					.setColor('#0099ff')
-					.setTitle(`Puntos de ${nick}`)
-					.addFields(fields);
-				message.channel.send(embed);
-			}
-		}, function(e) {
-			console.error(e);
-		});
-	}
-	else if (args[0] === 'listar') {
-		// Listar la tabla completa de puntos
-		googleSheet.getAllMembersData().then(function(val) {
-			let output = '';
-
-			Object.entries(val).forEach(([name, data]) => {
-				output += `${name} - ${data.total_points}\n`;
-			});
-
-			message.channel.send(output);
-		}, function(e) {
-			console.error(e);
-		});
-	}
-	else if (args[0] === 'limpiar') {
-		if(message.author.username === 'Deznait') {message.channel.bulkDelete(100, true);}
-	}
-	else {
-		// Recuperar los puntos de los miembros indicados
-		args.sort();
-
-		args.forEach(memberName => {
-			googleSheet.getMemberData(memberName).then(function(val) {
-				if(val) {
-					const output = `${memberName} - ${val.total_points}\n`;
-					message.channel.send(output);
+				else {
+					message.channel.send(`¡${message.author}, tienes ${val.total_points} puntos acumulados!`);
 				}
 			}, function(e) {
 				console.error(e);
 			});
-		});
+		}
+		else if (args[0] === 'detalle') {
+			google.getMemberData(nick).then(function(val) {
+				if(!val) {
+					message.channel.send(`${message.author}, no te hemos encontrado en la lista de puntos. :sob:`);
+				}
+				else {
+					const fields = [
+						{ name: 'Puntos totales', value: val.total_points, inline: true },
+						{ name: 'Puntos semanales', value: val.weekly_points, inline: true },
+					];
+
+					if(val.interview) {
+						fields.push({ name: 'Entrevista realizada', value: 'Sí' });
+					}
+
+					const embed = new Discord.MessageEmbed()
+						.setColor('#0099ff')
+						.setTitle(`Puntos de ${nick}`)
+						.addFields(fields);
+					message.channel.send(embed);
+				}
+			}, function(e) {
+				console.error(e);
+			});
+		}
+		else if (args[0] === 'listar') {
+			// Listar la tabla completa de puntos
+			google.getAllMembersData().then(function(val) {
+				let output = '';
+
+				val.forEach(data => {
+					output += `${data.name} - ${data.total_points}\n`;
+				});
+
+				message.channel.send(output);
+			}, function(e) {
+				console.error(e);
+			});
+		}
+		else if (args[0] === 'limpiar') {
+			if(message.author.username === 'Deznait') {message.channel.bulkDelete(100, true);}
+		}
+		else {
+			// Recuperar los puntos de los miembros indicados
+			args.sort();
+
+			args.forEach(memberName => {
+				google.getMemberData(memberName).then(function(val) {
+					if(val) {
+						const output = `${memberName} - ${val.total_points}\n`;
+						message.channel.send(output);
+					}
+				}, function(e) {
+					console.error(e);
+				});
+			});
+		}
 	}
 });
 
